dab05a9702eea2d76b2d839ebbd806beed43b386
Peter <pedromoyano454@gmail.com>
2024-03-18 18:40:30 -0300

    Add pages

---
+++ qa-app/qa-app/pages/clients-faq.js
@@ -0,0 +1,12 @@
+import Layout from "@/app/components/Layout";
+
+const ClientsFaq = () => {
+	return (
+		<Layout>
+			<h1>Clients FAQ</h1>
+			{/* Clients FAQ content */}
+		</Layout>
+	);
+}
+
+export default ClientsFaq;
---
+++ qa-app/qa-app/pages/useful-links.js
@@ -0,0 +1,12 @@
+import Layout from "@/app/components/Layout";
+
+const UsefulLinks = () => {
+	return (
+		<Layout>
+			<h1>Useful Links</h1>
+			{/* Useful links content */}
+		</Layout>
+	);
+}
+
+export default UsefulLinks;
