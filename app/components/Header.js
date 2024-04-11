05ae4ee3b665146743e67c2d4ebfe105529f6b71
Peter <pedromoyano454@gmail.com>
2024-04-10 22:39:34 -0300

    Style coverImage and globals

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,24 +5,26 @@ import solvdLogo from './solvd-logo.png';
 import styles from '@/styles/Header.module.css';
 
 export default function Header() {
-	return (
-		<>
-			<header className={styles.header}>
-				<Link href="/">
-					<Image src={solvdLogo} alt="Solvd logo" width={150} height={38} />
-				</Link>
-				<nav className={styles.nav}>
-					<Link href="/"><div className={styles.link}>Home</div></Link>
-					<Link href="/bench-practice"><div className={styles.link}>Bench Practice</div></Link>
-					<Link href="/webinars"><div className={styles.link}>Webinars</div></Link>
-					<Link href="/useful-links"><div className={styles.link}>Useful Links</div></Link>
-					<Link href="/clients-faq"><div className={styles.link}>Clients FAQ</div></Link>
-				</nav>
-			</header>
-			<div className={styles.coverImage} style={{ position: 'relative', height: '200px', width: '100%' }}>
-				<Image src={cover} alt="Solvd cover" layout="fill" objectFit="cover" />
-			</div>
+  return (
+    <>
+      <header className={styles.header}>
+        <nav className={styles.navbar}>
+          <Link href="/" className={styles.logo}>
+            <Image src={solvdLogo} alt="Solvd logo" width={150} height={38} />
+          </Link>
+          <div className={styles.buttons}>
+            <Link href="/"><div className={styles.button}>Home</div></Link>
+            <Link href="/bench-practice"><div className={styles.button}>Practice</div></Link>
+            {/* <Link href="/webinars"><div className={styles.button}>Webinars</div></Link> */}
+            <Link href="/useful-links"><div className={styles.button}>Useful Links</div></Link>
+            {/* <Link href="/clients-faq"><div className={styles.button}>Clients FAQ</div></Link> */}
+          </div>
+        </nav>
+      </header>
+      <div className={styles.coverImage}>
+        <Image src={cover} alt="Solvd cover" layout="fill" objectFit="cover" />
+      </div>
 
-		</>
+    </>
   );
 }
