218cd745044a177f0dc983901312085d8a874cbc
Peter <pedromoyano454@gmail.com>
2024-04-20 22:01:53 -0300

    Ensure consent screen

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,17 @@
 // This file will configure NextAuth to use Google for authentication
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-console.log("GoogleProvider: ", GoogleProvider);
 
 export default NextAuth({
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			authorization: {
+				params: {
+					prompt: 'consent',  // Ensures that the consent screen is shown
+				},
+			},
 		}),
 	],
 });
