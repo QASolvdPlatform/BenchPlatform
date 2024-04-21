74dfaf5b7ae908d7ea39cfea949ec0283d6c34ec
Peter <pedromoyano454@gmail.com>
2024-04-21 12:37:27 -0300

    Add login styling

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,30 +1,42 @@
 import Image from "next/image";
 import Logo from "@/public/images/Logo_solvd_RGB_Original.png";
 import { signIn } from "next-auth/react";
+import styles from "./index.module.css";
 
 const Login = () => {
     const redirectUrl = "/";
     return (
-        <div>
-            <Image src={Logo} />
-            <h1>Welcome to the Solvd QA Bench program</h1>
-            <div>
+        <div className={styles.bgImage}>
+            <div className={styles.card}>
                 <Image
-                    src="https://cdn.peopleforce.io/assets/integrations/google_workspace-db7f3b99932cd740a984148aa33fad3e634a2c6802a5d3d843d233debbca10a9.svg"
-                    width={25}
-                    height={25}
+                    className={styles.logo}
+                    src={Logo}
+                    alt="Solvd Logo"
+                    width={140}
+                    height={35}
                 />
-                <button
-                    onClick={() =>
-                        signIn("google", {
-                            prompt: "consent",
-                            redirect: false,
-                            callbackUrl: redirectUrl,
-                        })
-                    }
-                >
-                    Sign in with Google
-                </button>
+                <h2 className={styles.header}>
+                    Welcome to the Solvd QA Bench program
+                </h2>
+                <div>
+                    <button
+                        className={styles.button}
+                        onClick={() =>
+                            signIn("google", {
+                                prompt: "consent",
+                                redirect: false,
+                                callbackUrl: redirectUrl,
+                            })
+                        }
+                    >
+                        <Image
+                            src="https://cdn.peopleforce.io/assets/integrations/google_workspace-db7f3b99932cd740a984148aa33fad3e634a2c6802a5d3d843d233debbca10a9.svg"
+                            width={25}
+                            height={25}
+                        />
+                        Login with Google
+                    </button>
+                </div>
             </div>
         </div>
     );
